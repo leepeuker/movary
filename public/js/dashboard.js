fd76ac1afe9481e1ceb6552c20f9baeaeb6fad1d
Lee Peuker <lee.peuker@protonmail.com>
2022-05-29 19:05:30 +0200

    Update dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,11 +1,15 @@
 function toggleButton (element) {
-	if (element.nextElementSibling.style.display == "none") {
+	if (element.nextElementSibling.classList.contains("inactiveItem") == true) {
 		// element.classList.remove("active");
-		element.nextElementSibling.style.display = "";
-		element.style.backgroundColor = "#e9ecef";
+		element.nextElementSibling.classList.remove("inactiveItem");
+		element.nextElementSibling.classList.add("activeItem");
+		element.classList.remove("inactiveItemButton");
+		element.classList.add("activeItemButton");
 	} else {
 		// element.classList.add("active");
-		element.nextElementSibling.style.display = "none";
-		element.style.backgroundColor = "#fff";
+		element.nextElementSibling.classList.remove("activeItem");
+		element.nextElementSibling.classList.add("inactiveItem");
+		element.classList.remove("activeItemButton");
+		element.classList.add("inactiveItemButton");
 	}
 }
