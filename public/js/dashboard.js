7a1ac3cb71661903fddb4a744ae7bb35db3cda90
Lee Peuker <lee.peuker@protonmail.com>
2022-06-01 18:58:42 +0200

    Update dashboard with dropdown icons

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -5,11 +5,17 @@ function toggleButton (element) {
 		element.nextElementSibling.classList.add("activeItem");
 		element.classList.remove("inactiveItemButton");
 		element.classList.add("activeItemButton");
+
+		element.getElementsByClassName("bi")[0].classList.remove("bi-chevron-down");
+		element.getElementsByClassName("bi")[0].classList.add("bi-chevron-up");
 	} else {
 		// element.classList.add("active");
 		element.nextElementSibling.classList.remove("activeItem");
 		element.nextElementSibling.classList.add("inactiveItem");
 		element.classList.remove("activeItemButton");
 		element.classList.add("inactiveItemButton");
+
+		element.getElementsByClassName("bi")[0].classList.remove("bi-chevron-up");
+		element.getElementsByClassName("bi")[0].classList.add("bi-chevron-down");
 	}
 }
