0180e08629cd9f711b0aba9811e27d710ca46c40
Lee Peuker <lee.peuker@protonmail.com>
2022-07-22 18:46:36 +0200

    Add and fix letterboxd import code

--- src/Application/Service/Letterboxd/ImportRatings.php
+++ src/Application/Service/Letterboxd/ImportRatings.php
@@ -2,86 +2,94 @@
 
 namespace Movary\Application\Service\Letterboxd;
 
+use League\Csv\Reader;
 use Movary\Api;
+use Movary\Api\Letterboxd\WebScrapper;
+use Movary\Application\Movie;
+use Movary\Application\Service\Letterboxd\ValueObject\CsvLineRating;
+use Movary\Application\SyncLog;
+use Movary\ValueObject\PersonalRating;
+use Psr\Log\LoggerInterface;
 
 class ImportRatings
 {
     public function __construct(
-        // private readonly Application\Movie\Api $movieApi,
-        // private readonly WebScrapper $webScrapper,
-        // private readonly LoggerInterface $logger,
-        // private readonly Application\SyncLog\Repository $scanLogRepository
-    )
-    {
+        private readonly Movie\Api $movieApi,
+        private readonly WebScrapper $webScrapper,
+        private readonly LoggerInterface $logger,
+        private readonly SyncLog\Repository $scanLogRepository
+    ) {
     }
 
-    // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
     public function execute(int $userId, string $ratingsCsvPath, bool $verbose = false, bool $overwriteExistingData = false) : void
     {
-        // TODO fix this stuff
-        // $ratings = Reader::createFromPath($ratingsCsvPath);
-        // $ratings->setHeaderOffset(0);
-        //
-        // foreach ($ratings->getRecords() as $rating) {
-        //     $this->ensureValidCsvRow($rating);
-        //
-        //     $movie = $this->findMovieByLetterboxdUri($rating['Letterboxd URI']);
-        //
-        //     if ($movie === null) {
-        //         $this->logger->error('Could not find movie with uri: ' . $rating['Letterboxd URI']);
-        //
-        //         continue;
-        //     }
-        //
-        //     $ratingWithScale10 = $rating['Rating'] * 2;
-        //     $personalRating = PersonalRating::create((int)$ratingWithScale10);
-        //
-        //     if ($overwriteExistingData === false && $movie->getUserRating() !== null) {
-        //         $this->outputMessage("Ignoring {$movie->getTitle()} rating: " . $personalRating . PHP_EOL, $verbose);
-        //
-        //         continue;
-        //     }
-        //
-        //     $this->outputMessage("Updating {$movie->getTitle()} with rating: " . $personalRating . PHP_EOL, $verbose);
-        //
-        //     $this->movieApi->updateUserRating($movie->getId(), $personalRating);
-        // }
-        //
-        // $this->scanLogRepository->insertLogForLetterboxdSync();#
+        $ratings = Reader::createFromPath($ratingsCsvPath);
+        $ratings->setHeaderOffset(0);
+
+        foreach ($ratings->getRecords() as $rating) {
+            $this->ensureValidCsvRow($rating);
+            $csvLineRating = CsvLineRating::createFromCsvLine($rating);
+
+            $movie = $this->findMovieByLetterboxdUri($csvLineRating->getLetterboxdUri());
+
+            if ($movie === null) {
+                $this->logger->info('Movie not in history: ' . $csvLineRating->getName());
+
+                continue;
+            }
+
+            $userRating = $csvLineRating->getRating() * 2;
+            $personalRating = PersonalRating::create((int)$userRating);
+
+            if ($overwriteExistingData === false && $this->movieApi->findUserRating($movie->getId(), $userId) !== null) {
+                $this->logger->info('Ignoring rating for movie: ' . $csvLineRating->getLetterboxdUri());
+
+                $this->outputMessage("Ignoring {$movie->getTitle()} rating: " . $personalRating . PHP_EOL, $verbose);
+
+                continue;
+            }
+
+            $this->logger->info("Updating {$movie->getTitle()} with rating: " . $personalRating);
+            $this->outputMessage("Updating {$movie->getTitle()} with rating: " . $personalRating . PHP_EOL, $verbose);
+
+            $this->movieApi->updateUserRating($movie->getId(), $userId, $personalRating);
+        }
+
+        $this->scanLogRepository->insertLogForLetterboxdSync();#
 
         unlink($ratingsCsvPath);
     }
 
-    // public function findMovieByLetterboxdUri(string $letterboxdUri) : ?Application\Movie\Entity
-    // {
-    //     $letterboxdId = basename($letterboxdUri);
-    //     $movie = $this->movieApi->findByLetterboxdId($letterboxdId);
-    //
-    //     if ($movie === null) {
-    //         $tmdbId = $this->webScrapper->getProviderTmdbId($letterboxdUri);
-    //
-    //         $movie = $this->movieApi->findByTmdbId($tmdbId);
-    //         if ($movie === null) {
-    //             return null;
-    //         }
-    //
-    //         $this->movieApi->updateLetterboxdId($movie->getId(), $letterboxdId);
-    //     }
-    //
-    //     return $movie;
-    // }
-    //
-    // private function ensureValidCsvRow(array $rating) : void
-    // {
-    //     if (empty($rating['Letterboxd URI']) === true || empty($rating['Rating']) === true) {
-    //         throw new \RuntimeException('Invalid csv row in letterboxed rating csv.');
-    //     }
-    // }
-    //
-    // private function outputMessage(string $message, bool $verbose) : void
-    // {
-    //     if ($verbose === true) {
-    //         echo $message;
-    //     }
-    // }
+    public function findMovieByLetterboxdUri(string $letterboxdUri) : ?Movie\Entity
+    {
+        $letterboxdId = basename($letterboxdUri);
+        $movie = $this->movieApi->findByLetterboxdId($letterboxdId);
+
+        if ($movie === null) {
+            $tmdbId = $this->webScrapper->getProviderTmdbId($letterboxdUri);
+
+            $movie = $this->movieApi->findByTmdbId($tmdbId);
+            if ($movie === null) {
+                return null;
+            }
+
+            $this->movieApi->updateLetterboxdId($movie->getId(), $letterboxdId);
+        }
+
+        return $movie;
+    }
+
+    private function ensureValidCsvRow(array $rating) : void
+    {
+        if (empty($rating['Letterboxd URI']) === true || empty($rating['Rating']) === true) {
+            throw new \RuntimeException('Invalid csv row in letterboxed rating csv.');
+        }
+    }
+
+    private function outputMessage(string $message, bool $verbose) : void
+    {
+        if ($verbose === true) {
+            echo $message;
+        }
+    }
 }
--- src/Application/Service/Letterboxd/ImportWatchedMovies.php
+++ src/Application/Service/Letterboxd/ImportWatchedMovies.php
@@ -2,11 +2,64 @@
 
 namespace Movary\Application\Service\Letterboxd;
 
+use League\Csv\Reader;
+use Movary\Api\Letterboxd\WebScrapper;
+use Movary\Application\Movie;
+use Movary\Application\Service\Letterboxd\ValueObject\CsvLineHistory;
+use Movary\Application\Service\Tmdb;
+use Psr\Log\LoggerInterface;
+
 class ImportWatchedMovies
 {
+    public function __construct(
+        private readonly Movie\Api $movieApi,
+        private readonly WebScrapper $webScrapper,
+        private readonly LoggerInterface $logger,
+        private readonly Tmdb\SyncMovie $tmdbMovieSync
+    ) {
+    }
+
     // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter
-    public function execute(int $userId, string $historyCsvPath) : void
+    public function execute(int $userId, string $historyCsvPath, bool $overwriteExistingData = false) : void
     {
+        $watchDates = Reader::createFromPath($historyCsvPath);
+        $watchDates->setHeaderOffset(0);
+
+        foreach ($watchDates->getRecords() as $watchDate) {
+            $csvLineHistory = CsvLineHistory::createFromCsvLine($watchDate);
+
+            $movie = $this->fetchMovieByLetterboxdUri($csvLineHistory->getLetterboxdUri());
+
+            if ($overwriteExistingData === false && $this->movieApi->findUserRating($movie->getId(), $userId) !== null) {
+                $this->logger->info('Ignoring already existing watch date for movie: ' . $movie->getTitle());
+
+                continue;
+            }
+
+            $this->movieApi->replaceHistoryForMovieByDate($movie->getId(), $userId, $csvLineHistory->getDate(), 1);
+            $this->logger->info(sprintf('Imported watch date for movie "%s": %s', $csvLineHistory->getName(), $csvLineHistory->getDate()));
+        }
+
         unlink($historyCsvPath);
+    }
+
+    public function fetchMovieByLetterboxdUri(string $letterboxdUri) : Movie\Entity
+    {
+        $letterboxdId = basename($letterboxdUri);
+        $movie = $this->movieApi->findByLetterboxdId($letterboxdId);
+
+        if ($movie === null) {
+            $tmdbId = $this->webScrapper->getProviderTmdbId($letterboxdUri);
+
+            $movie = $this->movieApi->findByTmdbId($tmdbId);
+
+            if ($movie === null) {
+                $movie = $this->tmdbMovieSync->syncMovie($tmdbId);
+            }
+
+            $this->movieApi->updateLetterboxdId($movie->getId(), $letterboxdId);
+        }
+
+        return $movie;
     }
 }
