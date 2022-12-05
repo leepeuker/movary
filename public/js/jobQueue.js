9ebc9db9e218c2c97257516e4aea225b4f003b4c
Lee Peuker <lee.peuker@protonmail.com>
2022-12-05 19:37:31 +0100

    Extend frontend

---
+++ public/js/jobQueue.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let url = new URL(window.location.href)
+    let params = new URLSearchParams(url.search);
+    let jpp = params.get('jpp')
+
+    if (jpp != null) {
+        document.getElementById('jobsPerPage').value = jpp
+
+        return
+    }
+
+    document.getElementById('jobsPerPage').value = 30
+});
+
+function refreshPage() {
+    const jobsPerPage = document.getElementById('jobsPerPage').value
+
+    window.location.href = '/job-queue?jpp=' + jobsPerPage
+}
