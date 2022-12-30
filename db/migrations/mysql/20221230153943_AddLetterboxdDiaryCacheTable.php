622ed6f9d142f0e5c32d8f24e9c0516772ad17e0
Lee Peuker <lee.peuker@protonmail.com>
2022-12-30 19:37:05 +0100

    Increase diary id size

--- db/migrations/mysql/20221230153943_AddLetterboxdDiaryCacheTable.php
+++ db/migrations/mysql/20221230153943_AddLetterboxdDiaryCacheTable.php
@@ -14,7 +14,7 @@ final class AddLetterboxdDiaryCacheTable extends AbstractMigration
         $this->execute(
             <<<SQL
             CREATE TABLE `cache_letterboxd_diary` (
-                `diary_id` VARCHAR(5) NOT NULL,
+                `diary_id` VARCHAR(255) NOT NULL,
                 `letterboxd_id` VARCHAR(4) NOT NULL,
                 PRIMARY KEY (`diary_id`)
             )
