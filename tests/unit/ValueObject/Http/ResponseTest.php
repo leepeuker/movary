cda7514969384b7b833a1491e6b1a5c3b6b845af
Lee Peuker <lee.peuker@protonmail.com>
2023-01-06 13:07:28 +0100

    Add unit test for http response

---
+++ tests/unit/ValueObject/Http/ResponseTest.php
@@ -0,0 +1,77 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Unit\Movary\ValueObject\Http;
+
+use Movary\ValueObject\Http\Header;
+use Movary\ValueObject\Http\Response;
+use Movary\ValueObject\Http\StatusCode;
+use PHPUnit\Framework\TestCase;
+
+/** @covers \Movary\ValueObject\Http\Response */
+class ResponseTest extends TestCase
+{
+    public function testCreateBadRequest() : void
+    {
+        self::assertEquals(
+            Response::create(StatusCode::createBadRequest()),
+            Response::createBadRequest(),
+        );
+    }
+
+    public function testCreateCsv() : void
+    {
+        self::assertEquals(
+            Response::create(StatusCode::createOk(), 'foobar', [Header::createContentTypeCsv()]),
+            Response::createCsv('foobar'),
+        );
+    }
+
+    public function testCreateForbidden() : void
+    {
+        self::assertEquals(
+            Response::create(StatusCode::createForbidden()),
+            Response::createForbidden(),
+        );
+    }
+
+    public function testCreateJson() : void
+    {
+        self::assertEquals(
+            Response::create(StatusCode::createOk(), 'foobar', [Header::createContentTypeJson()]),
+            Response::createJson('foobar'),
+        );
+    }
+
+    public function testCreateNotFound() : void
+    {
+        self::assertEquals(
+            Response::create(StatusCode::createNotFound()),
+            Response::createNotFound(),
+        );
+    }
+
+    public function testCreateOk() : void
+    {
+        self::assertEquals(
+            Response::create(StatusCode::createOk()),
+            Response::createOk(),
+        );
+    }
+
+    public function testCreateSeeOther() : void
+    {
+        self::assertEquals(
+            Response::create(StatusCode::createSeeOther(), null, [Header::createLocation('/foobar')]),
+            Response::createSeeOther('/foobar'),
+        );
+    }
+
+    public function testGetters() : void
+    {
+        $subject = Response::create(StatusCode::createOk(), 'foobar', [Header::createContentTypeJson()]);
+
+        self::assertSame($subject->getBody(), 'foobar');
+        self::assertEquals($subject->getStatusCode(), StatusCode::createOk());
+        self::assertEquals($subject->getHeaders(), [Header::createContentTypeJson()]);
+    }
+}
