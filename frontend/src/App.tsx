b84ccf998306c8047e0e2126dada3cfe0cc7b769
Pere Busquets <43433557+perebusquets@users.noreply.github.com>
2023-01-20 01:36:11 +0100

    Add router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Login from './pages/Login/Login'
 import { Suspense } from 'react'
 import Loader from './components/Loader'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const App = () => {
   const queryClient = new QueryClient({
@@ -13,11 +14,18 @@ const App = () => {
     },
   })
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+  ]);
+
   return (
     <MantineProvider withNormalizeCSS withGlobalStyles>
       <Suspense fallback={<Loader/>}>
         <QueryClientProvider client={queryClient}>
-          <Login />
+          <RouterProvider router={router} />
         </QueryClientProvider>
       </Suspense>
     </MantineProvider>
