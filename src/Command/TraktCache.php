0dc49cee8512068069591996383810e419977be6
Lee Peuker <lee.peuker@protonmail.com>
2023-01-21 11:51:01 +0100

    Add command to update only the trakt cache timestamps

---
+++ src/Command/TraktCache.php
@@ -0,0 +1,54 @@
+<?php declare(strict_types=1);
+
+namespace Movary\Command;
+
+use Movary\Service\Trakt\ImportRatings;
+use Movary\Service\Trakt\ImportWatchedMovies;
+use Movary\Service\Trakt\TraktCacheService;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Throwable;
+
+class TraktCache extends Command
+{
+    private const OPTION_NAME_USER_ID = 'userId';
+
+    protected static $defaultName = 'trakt:updateCache';
+
+    public function __construct(
+        private readonly ImportRatings $importRatings,
+        private readonly ImportWatchedMovies $importWatchedMovies,
+        private readonly TraktCacheService $traktCacheService,
+        private readonly LoggerInterface $logger,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure() : void
+    {
+        $this->setDescription('Update the local trakt cache timestamps with the remote state.')
+            ->addOption(self::OPTION_NAME_USER_ID, [], InputOption::VALUE_REQUIRED, 'Id of user.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) : int
+    {
+        $userId = (int)$input->getOption(self::OPTION_NAME_USER_ID);
+        if (empty($userId) === true) {
+            $this->generateOutput($output, 'Missing option --userId');
+            exit;
+        }
+
+        try {
+            $this->traktCacheService->updateCache($userId);
+        } catch (Throwable $t) {
+            $this->generateOutput($output, 'ERROR: Could not complete trakt import.');
+            $this->logger->error('Could not complete trakt mo.', ['exception' => $t]);
+
+            return Command::FAILURE;
+        }
+
+        return Command::SUCCESS;
+    }
+}
