47a06da0b3faf0716ed9a4d8dc5e78d5a01ea8e1
Lee Peuker <lee.peuker@protonmail.com>
2023-06-12 14:59:51 +0200

    Improve db migrations

---
+++ db/migrations/mysql/20230507185125_AddDashboardSettingsToUserTable.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class AddDashboardSettingsToUserTable extends AbstractMigration
+{
+    public function down() : void
+    {
+        $this->execute(
+            <<<SQL
+            ALTER TABLE user DROP COLUMN dashboard_extended_rows;
+            ALTER TABLE user DROP COLUMN dashboard_visible_rows;
+            SQL,
+        );
+    }
+
+    public function up() : void
+    {
+        $this->execute(
+            <<<SQL
+            ALTER TABLE user ADD COLUMN dashboard_visible_rows TEXT NULL AFTER is_admin;
+            ALTER TABLE user ADD COLUMN dashboard_extended_rows TEXT NULL AFTER dashboard_visible_rows;
+            SQL,
+        );
+    }
+}
