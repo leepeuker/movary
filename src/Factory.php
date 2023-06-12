ac35464d29b4ebc079004e1152727e685b1f837e
Lee Peuker <lee.peuker@protonmail.com>
2023-06-12 17:56:06 +0200

    Formatting

--- src/Factory.php
+++ src/Factory.php
@@ -52,13 +52,21 @@ use Twig;
 class Factory
 {
     private const SRC_DIRECTORY_NAME = 'src';
+
     private const DEFAULT_MIN_RUNTIME_IN_SECONDS_FOR_JOB_PROCESSING = 15;
+
     private const DEFAULT_DATABASE_MYSQL_CHARSET = 'utf8mb4';
+
     private const DEFAULT_DATABASE_MYSQL_PORT = 3306;
+
     private const DEFAULT_LOG_LEVEL = LogLevel::WARNING;
+
     private const DEFAULT_APPLICATION_VERSION = 'unknown';
+
     private const DEFAULT_TMDB_IMAGE_CACHING = false;
+
     private const DEFAULT_LOG_ENABLE_STACKTRACE = false;
+
     private const DEFAULT_ENABLE_FILE_LOGGING = true;
 
     public static function createConfig() : Config
