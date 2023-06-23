e7b5f7b454c86bba44a7aaa4e5ebb06dd35cb77a
Lee Peuker <lee.peuker@protonmail.com>
2023-06-23 13:15:21 +0200

    Add movieIds filter argument to imdb sync cli command

--- src/Domain/Movie/MovieApi.php
+++ src/Domain/Movie/MovieApi.php
@@ -162,9 +162,9 @@ class MovieApi
         return $this->historyApi->fetchHistoryOrderedByWatchedAtDesc($userId);
     }
 
-    public function fetchMovieIdsHavingImdbIdOrderedByLastImdbUpdatedAt(?int $maxAgeInHours = null, ?int $limit = null) : array
+    public function fetchMovieIdsHavingImdbIdOrderedByLastImdbUpdatedAt(?int $maxAgeInHours = null, ?int $limit = null, ?array $filterMovieIds = null) : array
     {
-        return $this->movieRepository->fetchMovieIdsHavingImdbIdOrderedByLastImdbUpdatedAt($maxAgeInHours, $limit);
+        return $this->movieRepository->fetchMovieIdsHavingImdbIdOrderedByLastImdbUpdatedAt($maxAgeInHours, $limit, $filterMovieIds);
     }
 
     public function fetchUniqueMovieGenres(int $userId) : array
--- src/Service/Imdb/ImdbMovieRatingSync.php
+++ src/Service/Imdb/ImdbMovieRatingSync.php
@@ -44,6 +44,7 @@ class ImdbMovieRatingSync
             $this->logger->debug('IMDb: Skipped updating not changed movie rating', [$this->generateMovieLogData($movie)]);
 
             $this->movieApi->updateImdbTimestamp($movieId);
+
             return;
         }
 
@@ -68,14 +69,15 @@ class ImdbMovieRatingSync
     public function syncMultipleMovieRatings(
         ?int $maxAgeInHours = null,
         ?int $movieCountSyncThreshold = null,
+        array $movieIds = null,
         int $minDelayBetweenRequests = self::DEFAULT_MIN_DELAY_BETWEEN_REQUESTS_IN_MS,
     ) : void {
-        $movieIds = $this->movieApi->fetchMovieIdsHavingImdbIdOrderedByLastImdbUpdatedAt($maxAgeInHours, $movieCountSyncThreshold);
+        $movieIds = $this->movieApi->fetchMovieIdsHavingImdbIdOrderedByLastImdbUpdatedAt($maxAgeInHours, $movieCountSyncThreshold, $movieIds);
 
         foreach ($movieIds as $index => $movieId) {
             $this->syncMovieRating($movieId);
 
-            if ($index === array_key_last($movieIds)) {
+            if ($index === array_key_last($movieIds) || ((int)$movieCountSyncThreshold !== 0 && (int)$index + 1 >= $movieCountSyncThreshold)) {
                 break;
             }
 
