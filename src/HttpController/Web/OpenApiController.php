e956f27c6c25698a277411539762ffa473092900
Lee Peuker <lee.peuker@protonmail.com>
2023-09-02 18:10:11 +0200

    Use application url for swagger ui

--- src/HttpController/Web/OpenApiController.php
+++ src/HttpController/Web/OpenApiController.php
@@ -2,6 +2,7 @@
 
 namespace Movary\HttpController\Web;
 
+use Movary\Service\ServerSettings;
 use Movary\ValueObject\Http\Response;
 use Movary\ValueObject\Http\StatusCode;
 use Twig\Environment;
@@ -10,14 +11,24 @@ class OpenApiController
 {
     public function __construct(
         private readonly Environment $twig,
+        private readonly ServerSettings $serverSettings,
     ) {
     }
 
     public function renderPage() : Response
     {
+        $openAiJsonUrl = '/api/openapi.json';
+
+        $applicationUrl = $this->serverSettings->getApplicationUrl();
+        if ($applicationUrl !== null) {
+            $openAiJsonUrl = trim($applicationUrl, '/') . $openAiJsonUrl;
+        }
+
         return Response::create(
             StatusCode::createOk(),
-            $this->twig->render('page/api.html.twig'),
+            $this->twig->render('page/api.html.twig', [
+                'openAiJsonUrl' => $openAiJsonUrl
+            ]),
         );
     }
 }
