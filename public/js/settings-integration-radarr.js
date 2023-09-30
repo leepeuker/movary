8a4327fe13768f8f89aa6c76876702ae93645261
Lee Peuker <lee.peuker@protonmail.com>
2023-09-30 19:58:55 +0200

    Improve settings UI

--- public/js/settings-integration-radarr.js
+++ public/js/settings-integration-radarr.js
@@ -1,24 +1,44 @@
 async function regenerateRadarrFeedId() {
-    await fetch('/api/radarr/regeneratefeedid', {'method': 'put'}).then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`)
-        } else {
-            return response.json();
-        }
-    }).then(radarrFeedUrl => {
-        document.getElementById('radarrFeedUrl').innerText = radarrFeedUrl.url;
-        addAlert('alertFeedUrlDiv', 'Generated new feed url', 'success');
-    });
+    removeAlert('alertFeedUrlDiv')
 
+    regenerateRadarrFeedRequest().then(webhookUrl => {
+        setRadarrFeedUrl(webhookUrl)
+        addAlert('alertFeedUrlDiv', 'Generated new feed url', 'success')
+        document.getElementById('deleteRadarrFeedButton').classList.remove('disabled')
+    }).catch((error) => {
+        console.log(error)
+        addAlert('alertFeedUrlDiv', 'Could not generate feed url', 'danger')
+    })
 }
 
-async function deleteRadarrFeedId()
-{
+async function regenerateRadarrFeedRequest() {
+    const response = await fetch('/api/radarr/regeneratefeedid', {'method': 'put'})
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+    }
+    const data = await response.json()
+
+    return data.url
+}
+
+async function deleteRadarrFeedId() {
     await fetch('/api/radarr/deletefeedid', {'method': 'delete'}).then(response => {
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`)
-        } else {
-            addAlert('alertFeedUrlDiv', 'Deleted new feed url', 'success');
         }
+
+        setRadarrFeedUrl()
+        addAlert('alertFeedUrlDiv', 'Deleted feed url', 'success');
     });
+}
+
+function setRadarrFeedUrl(webhookUrl) {
+    if (webhookUrl) {
+        document.getElementById('radarrFeedUrl').innerHTML = webhookUrl
+        document.getElementById('deleteRadarrFeedButton').classList.remove('disabled')
+    } else {
+        document.getElementById('radarrFeedUrl').innerHTML = '-'
+        document.getElementById('deleteRadarrFeedButton').classList.add('disabled')
+    }
 }
