7d3301a26b7235220a251443a64c3a5a4ab6630f
JVT038 <47184046+JVT038@users.noreply.github.com>
2024-01-26 12:31:01 +0100

    Fix migration for sqlite

--- db/migrations/sqlite/20240126110433_add_device_name_to_auth_token_table.php
+++ db/migrations/sqlite/20240126110433_add_device_name_to_auth_token_table.php
@@ -10,39 +10,41 @@ final class AddDeviceNameToAuthTokenTable extends AbstractMigration
     {
         $this->execute(
             <<<SQL
-            CREATE TABLE `user_api_token_old` (
-                `user_id` INT(10) NOT NULL,
-                `token` CHAR(36) NOT NULL,
+            CREATE TABLE `user_auth_token` (
+                `id` INTEGER NOT NULL,
+                `user_id` INTEGER NOT NULL,
+                `token` TEXT NOT NULL,
+                `expiration_date` TEXT NOT NULL,
                 `created_at` TEXT NOT NULL,
-                PRIMARY KEY (`token`),
-                UNIQUE (`user_id`),
-                FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE
+                PRIMARY KEY (`id`),
+                FOREIGN KEY (`user_id`) REFERENCES user (`id`) ON DELETE CASCADE
             )
             SQL,
         );
-        $this->execute('INSERT INTO `user_api_token_old` (`user_id`, `token`, `created_at`) SELECT `user_id`, `token`, `created_at` FROM `user_api_token`');
-        $this->execute('DROP TABLE `user_api_token`');
-        $this->execute('ALTER TABLE `user_api_token_old` RENAME TO `user_api_token`');
+        $this->execute('INSERT INTO `user_auth_token_old` (`id`, `user_id`, `token`, `expiration_date`, `created_at`) SELECT `id`, `user_id`, `token`, `expiration_date`, `created_at` FROM `user_auth_token`');
+        $this->execute('DROP TABLE `user_auth_token`');
+        $this->execute('ALTER TABLE `user_auth_token_old` RENAME TO `user_auth_token`');
     }
 
     public function up() : void
     {
         $this->execute(
             <<<SQL
-            CREATE TABLE `user_api_token_new` (
+            CREATE TABLE `user_auth_token_new` (
+                `id` INTEGER NOT NULL,
                 `user_id` INT(10) NOT NULL,
                 `token` CHAR(36) NOT NULL,
                 `device_name` VARCHAR(256) NOT NULL,
                 `user_agent_string` VARCHAR(256) NOT NULL,
+                `expiration_date` TEXT NOT NULL,
                 `created_at` TEXT NOT NULL,
-                PRIMARY KEY (`token`),
-                UNIQUE (`user_id`),
-                FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE
+                PRIMARY KEY (`id`),
+                FOREIGN KEY (`user_id`) REFERENCES user (`id`) ON DELETE CASCADE
             )
             SQL,
         );
-        $this->execute('INSERT INTO `user_api_token_new` (`user_id`, `token`, `created_at`) SELECT * FROM `user_api_token`');
-        $this->execute('DELETE `user_api_token`');
-        $this->execute('ALTER TABLE `user_api_token_new` RENAME TO `user_api_token`');
+        $this->execute('INSERT INTO `user_auth_token_new` (`id`, `user_id`, `token`, `expiration_date`, `created_at`) SELECT * FROM `user_auth_token`');
+        $this->execute('DROP TABLE `user_auth_token`');
+        $this->execute('ALTER TABLE `user_auth_token_new` RENAME TO `user_auth_token`');
     }
 }
